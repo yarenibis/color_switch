629a7474b295250db5018ae9c18dfcb4fb075adc
yarenibis <yarenibis150@gmail.com>
2025-12-02 21:50:14 +0300

    camera and world component

---
+++ lib/my_game.dart
@@ -0,0 +1,46 @@
+import 'package:color_switch/player.dart';
+import 'package:flame/components.dart';
+import 'dart:ui';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+
+
+class MyGame extends FlameGame with TapCallbacks{
+  late Player myPlayer;
+
+   MyGame()
+      : super(
+          camera: CameraComponent.withFixedResolution(
+            width: 600,
+            height: 1000,
+          ),
+        ); 
+        
+  @override
+  Color backgroundColor ()=>const Color(0xff222222);
+
+  @override
+  void onMount() {
+    world.add(myPlayer = Player());
+    super.onMount();
+  }
+
+
+@override
+  void update(double dt) {
+    final cameraY = camera.viewfinder.position.y;
+    final playerY = myPlayer.position.y;
+
+    if (playerY < cameraY) {
+      camera.viewfinder.position = Vector2(0, playerY);
+    }
+    super.update(dt);
+  }
+
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    myPlayer.jump();
+    super.onTapDown(event);
+  }
+}
